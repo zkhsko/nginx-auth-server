b7441e0f92d85c39cd50bacdc00564ce6f2d7f85
dongpo.li <dongpo.li@hotmail.com>
2025-06-26 18:30:53 +0800

    添加OrderRefundInfo

--- src/main/java/org/nginx/auth/service/OrderRefundInfoService.java
+++ src/main/java/org/nginx/auth/service/OrderRefundInfoService.java
@@ -3,9 +3,10 @@ package org.nginx.auth.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderPaymentInfoService {
+public class OrderRefundInfoService {
 
     public void refundByAdmin(String orderId, Long orderRefundAmount, String refundReason) {
+
 
 
 
